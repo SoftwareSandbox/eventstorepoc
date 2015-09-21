0d9c0d5916733d2bacd84eab67c1c342299010d5
Sch3lp <sch3lp@gmail.com>
2015-09-21 22:40:30 +0200

    working example of AtomPoster with Jackson

--- src/main/java/be/swsb/poster/AtomEvent.java
+++ src/main/java/be/swsb/poster/AtomEvent.java
@@ -1,5 +1,7 @@
 package be.swsb.poster;
 
+import java.util.UUID;
+
 /**
  * Created by sch3lp on 21/09/15.
  */
@@ -7,17 +9,24 @@ public class AtomEvent {
 
     private String stream;
     private Payload payload;
+    private UUID uuid;
 
     public AtomEvent(String stream, Payload payload) {
+        this.uuid = UUID.randomUUID();
         this.stream = stream;
         this.payload = payload;
     }
 
-    public String getStream() {
+    public String getEventType() {
         return stream;
     }
 
     public Payload getPayload() {
         return payload;
     }
+
+    public UUID getUUID() {
+        return uuid;
+    }
+
 }
--- src/main/java/be/swsb/poster/AtomPoster.java
+++ src/main/java/be/swsb/poster/AtomPoster.java
@@ -2,10 +2,12 @@ package be.swsb.poster;
 
 import org.glassfish.jersey.client.JerseyClient;
 import org.glassfish.jersey.client.JerseyClientBuilder;
-import org.glassfish.jersey.client.JerseyWebTarget;
+import org.glassfish.jersey.jackson.JacksonFeature;
 
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
 
 public class AtomPoster {
 
@@ -15,14 +17,19 @@ public class AtomPoster {
 
     public static void main(String[] args){
         AtomPoster atomPoster = new AtomPoster();
-        atomPoster.post(new AtomEvent("newstream", new Payload("1", "test")));
+        atomPoster.post(new AtomEvent("MyEventType", new Payload("2", "test")));
     }
 
     private void post(AtomEvent atomEvent) {
-        JerseyClient jerseyClient = new JerseyClientBuilder().build();
-        jerseyClient
-                .target("http://localhost:2113/streams/" + atomEvent.getStream())
+        JerseyClient jerseyClient = new JerseyClientBuilder()
+                .register(JacksonFeature.class)
+                .build();
+        Response response = jerseyClient
+                .target("http://localhost:2113/streams/"+atomEvent.getEventType())
                 .request(MediaType.APPLICATION_JSON_TYPE)
+                .header("ES-EventId", atomEvent.getUUID())
+                .header("ES-EventType", atomEvent.getEventType())
                 .post(Entity.json(atomEvent.getPayload()));
+        System.out.println(response);
     }
 }
